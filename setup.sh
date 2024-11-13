ef5d8d283f08162a637688b90ea6f48e54fe9111
AstrologSecra <astrologworkmail@gmail.com>
2024-11-13 10:09:05 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Предупреждение
-echo "Warning: This script will run script.sh и reloader.sh."
-echo "Make sure you understand the consequences of running these scripts."
+echo "Warning: This script will run script.sh."
+echo "Make sure you understand the consequences of running these script."
 read -p "Are you sure you want to continue? (y/n): " confirm
 
 if [[ "$confirm" != "y" ]]; then
@@ -11,16 +11,14 @@ if [[ "$confirm" != "y" ]]; then
 fi
 
 # Установка прав на исполнение для script.sh и art.sh
-echo "Preparring script.sh и reloader.sh..."
+echo "Preparring script.sh..."
 chmod +x script.sh
-chmod +x reloader.sh
+
 
 # Запуск script.sh
 echo "Start script.sh..."
 ./script.sh
 
-# Запуск art.sh
-echo "Start reloader.sh..."
-./reloader.sh
+
 
 echo "Completed."
